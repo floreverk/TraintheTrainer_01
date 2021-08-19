b33a127e71fe8c42f77d853349dbee0f1881f70b
Flore <78723853+floreverk@users.noreply.github.com>
2021-08-19 11:49:17 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -3,7 +3,17 @@ from PIL import ImageTk, Image
 from tkinter import font
 
 def start():
-    print("ok")
+    choice = [var1.get(), var2.get(), var3.get()]
+    if tuple(choice) == ('postcard1', '', ''):
+        #create postcard1
+    elif tuple(choice) == ('', 'postcard2', ''):
+        #create postcard2
+    elif tuple(choice) == ('', '', 'postcard3'):
+        #create postcard3
+    else:
+        error = Label(text="Please select only one postcard", bg="#fffedd")
+        error.grid(row=13, column=1)
+
 
 postcardmaker = Tk()
 postcardmaker.configure(bg="#fffedd")
@@ -36,13 +46,14 @@ var1 = StringVar()
 var2 = StringVar()
 var3 = StringVar()
 
-first_card = Checkbutton(postcardmaker, text="", variable="var1", onvalue="postcard1", offvalue="", bg="#fffedd")
-second_card = Checkbutton(postcardmaker, text="", variable="var2", onvalue="postcard2", offvalue="", bg="#fffedd")
-third_card = Checkbutton(postcardmaker, text="", variable="var3", onvalue="postcard3", offvalue="", bg="#fffedd")
+first_card = Checkbutton(postcardmaker, text="", variable=var1, onvalue="postcard1", offvalue="", bg="#fffedd")
+second_card = Checkbutton(postcardmaker, text="", variable=var2, onvalue="postcard2", offvalue="", bg="#fffedd")
+third_card = Checkbutton(postcardmaker, text="", variable=var3, onvalue="postcard3", offvalue="", bg="#fffedd")
 
 first_card.grid(row=2, column=0)
 second_card.grid(row=2, column=1)
 third_card.grid(row=2, column=2)
+
 
 recipient_name = Label(text="Name:", fg="#583b2a", bg="#fffedd")
 recipient_name.grid(row=4, column=0)
@@ -76,11 +87,10 @@ landenlijst.set('Choose country')
 menu = OptionMenu(postcardmaker, landenlijst, "Belgium", "France", "Germany")
 menu.grid(row=9, column=1)
 
-message = Entry(postcardmaker)
-message.grid(row=11, column=0, ipadx=150, ipady=50, columnspan = 3, pady=10)
+message = Text(postcardmaker, height=10)
+message.grid(row=11, column=0, columnspan=3)
 
 create_postcard = Button(text="Create Postcard", padx=10, command=start)
 create_postcard.grid(row=12, column=1)
 
 postcardmaker.mainloop()
-
